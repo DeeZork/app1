e90f9f2cb8df0008b898325eb231bc61747c2988
DeeZork <DeeZorks@gmail.com>
2023-08-20 20:04:11 +0300

    Задачи модуля 21

--- app/src/main/java/com/deezork/app1/MainActivity.kt
+++ app/src/main/java/com/deezork/app1/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        binding.bottomNavigation.setOnMenuItemClickListener {
+        binding.bottomNavigation.setOnNavigationItemSelectedListener{
 
             when (it.itemId) {
                 R.id.favorites -> {
@@ -40,20 +40,5 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
-//        binding.button.setOnClickListener {
-//            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
-//        }
-//        binding.button2.setOnClickListener {
-//            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-//        }
-//        binding.button3.setOnClickListener {
-//            Toast.makeText(this, "Смотреть позже", Toast.LENGTH_SHORT).show()
-//        }
-//        binding.button4.setOnClickListener {
-//            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-//        }
-//        binding.button5.setOnClickListener {
-//            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-//        }
     }
 }
