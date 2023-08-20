82ad5a89c1a6f2c1f2eaf9f7240565a891bac1e1
DeeZork <DeeZorks@gmail.com>
2023-08-20 19:43:10 +0300

    Задачи модуля 21

--- app/src/main/java/com/deezork/app1/MainActivity.kt
+++ app/src/main/java/com/deezork/app1/MainActivity.kt
@@ -12,20 +12,48 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        binding.button.setOnClickListener {
-            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        binding.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
         }
-        binding.button2.setOnClickListener {
-            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+
+        binding.bottomNavigation.setOnMenuItemClickListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Смотреть позже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
         }
-        binding.button3.setOnClickListener {
-            Toast.makeText(this, "Смотреть позже", Toast.LENGTH_SHORT).show()
-        }
-        binding.button4.setOnClickListener {
-            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-        }
-        binding.button5.setOnClickListener {
-            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-        }
+//        binding.button.setOnClickListener {
+//            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+//        }
+//        binding.button2.setOnClickListener {
+//            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+//        }
+//        binding.button3.setOnClickListener {
+//            Toast.makeText(this, "Смотреть позже", Toast.LENGTH_SHORT).show()
+//        }
+//        binding.button4.setOnClickListener {
+//            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+//        }
+//        binding.button5.setOnClickListener {
+//            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+//        }
     }
 }
