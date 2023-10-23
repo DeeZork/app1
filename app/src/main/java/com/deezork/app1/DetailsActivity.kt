30d1fb6055470dc8213eb489fb6db1c6d7bc8f57
DeeZork <DeeZorks@gmail.com>
2023-10-23 19:12:49 +0300

    Задачи модуля 24 раздел 2 полностью +класс Diff плюс Snack

--- app/src/main/java/com/deezork/app1/DetailsActivity.kt
+++ app/src/main/java/com/deezork/app1/DetailsActivity.kt
@@ -1,8 +1,10 @@
 package com.deezork.app1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.deezork.app1.databinding.ActivityDetailsBinding
+import com.google.android.material.snackbar.Snackbar
 
 class DetailsActivity : AppCompatActivity() {
     //Привязываем View из layout к переменным
@@ -13,12 +15,20 @@ class DetailsActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         setFilmsDetails()
+
+        val snackbar = Snackbar.make(binding.detailsLayout, "", Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction("В Избранное",{
+            Toast.makeText(this,"Фильм добавлен в Избранное",Toast.LENGTH_SHORT).show()
+        })
+//        snackbar.setAction("Посмотреть",{
+//            Toast.makeText(this,"Смотрим Фильм",Toast.LENGTH_SHORT).show()
+//        })
+        snackbar.show()
     }
 
     private fun setFilmsDetails() {
         //Получаем наш фильм из переданного бандла
         val film = intent.extras?.get("film") as Film
-
         //Устанавливаем заголовок
         binding.detailsToolbar.title = film.title
         //Устанавливаем картинку
