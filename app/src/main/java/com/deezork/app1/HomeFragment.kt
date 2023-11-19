85a21bf5ce86e415fda194d2c653d24bf7aee716
DeeZork <DeeZorks@gmail.com>
2023-11-19 21:56:47 +0300

    Задачи модуля 28 без *

--- app/src/main/java/com/deezork/app1/HomeFragment.kt
+++ app/src/main/java/com/deezork/app1/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.deezork.app1
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,12 +9,18 @@ import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.transition.Scene
+import androidx.transition.Slide
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import com.deezork.app1.databinding.FragmentHomeBinding
+import com.deezork.app1.databinding.MergeHomeScreenContentBinding
 import java.util.Locale
 
 
 class HomeFragment(val filmsDataBase: List<Film>) : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var bindingContent: MergeHomeScreenContentBinding
     private lateinit var filmsAdapter: FilmListRecyclerAdapter
 
     override fun onCreateView(
@@ -25,30 +32,40 @@ class HomeFragment(val filmsDataBase: List<Film>) : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeBinding.bind(view)
-        //находим наш RV
-        binding.mainRecycler.apply {
-            filmsAdapter =
-                FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {
-                    override fun click(film: Film) {
-                        (requireActivity() as MainActivity).launchDetailsFragment(film)
-                    }
-                })
-            //Присваиваем адаптер
-            adapter = filmsAdapter
-            //Присвои layoutmanager
-            layoutManager = LinearLayoutManager(requireContext())
-            //Применяем декоратор для отступов
-            val decorator = TopSpacingItemDecoration(8)
-            addItemDecoration(decorator)
+        bindingContent = MergeHomeScreenContentBinding.bind(view)
+        // Создаем сцену
+        val scene = Scene.getSceneForLayout(
+            binding.homeFragmentRoot,
+            R.layout.merge_home_screen_content,
+            requireContext()
+        )
+        //Создаем анимацию выезда поля поиска сверху
+        val searchSlide = Slide(Gravity.TOP).addTarget(R.id.search_view)
+//Создаем анимацию выезда RV снизу
+        val recyclerSlide = Slide(Gravity.BOTTOM).addTarget(R.id.main_recycler)
+//Создаем экземпляр TransitionSet, который объединит все наши анимации
+        val customTransition = TransitionSet().apply {
+            //Устанавливаем время, за которое будет проходить анимация
+            duration = 500
+            //Добавляем сами анимации
+            addTransition(recyclerSlide)
+            addTransition(searchSlide)
         }
-        //Кладем нашу БД в RV
-        filmsAdapter.addItems(filmsDataBase)
+//Также запускаем через TransitionManager, но вторым параметром передаем нашу кастомную анимацию
+        TransitionManager.go(scene, customTransition)
+
+        startSV()
+        startRV()
+    }
+
+    fun startSV() {
         // воспринимает клик в любой части searchView а не только на иконке
-        binding.searchView.setOnClickListener {
-            binding.searchView.isIconified = false
+        bindingContent.searchView.setOnClickListener {
+            bindingContent.searchView.isIconified = false
         }
+
         //Подключаем слушателя изменений введенного текста в поиска
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        bindingContent.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             //Этот метод отрабатывает при нажатии кнопки "поиск" на софт клавиатуре
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return true
@@ -64,14 +81,39 @@ class HomeFragment(val filmsDataBase: List<Film>) : Fragment() {
                 //Фильтруем список на поискк подходящих сочетаний
                 val result = filmsDataBase.filter {
                     //Чтобы все работало правильно, нужно и запрос, и имя фильма приводить к нижнему регистру
-                    it.title.toLowerCase(Locale.getDefault()).contains(newText?.toLowerCase(Locale.getDefault())
-                        .toString())
+                    it.title.toLowerCase(Locale.getDefault()).contains(
+                        newText?.toLowerCase(Locale.getDefault())
+                            .toString()
+                    )
                 }
                 //Добавляем в адаптер
                 filmsAdapter.addItems(result)
                 return true
             }
         })
+    }
+
+    fun startRV() {
+        //находим наш RV
+        bindingContent.mainRecycler.apply {
+            filmsAdapter =
+                FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {
+                    override fun click(film: Film) {
+                        (requireActivity() as MainActivity).launchDetailsFragment(film)
+                    }
+                })
+            //Присваиваем адаптер
+            adapter = filmsAdapter
+            //Присвои layoutmanager
+            layoutManager = LinearLayoutManager(requireContext())
+            //Применяем декоратор для отступов
+            val decorator = TopSpacingItemDecoration(8)
+            addItemDecoration(decorator)
+
+        }
+        //Кладем нашу БД в RV
+        filmsAdapter.addItems(filmsDataBase)
+
     }
 
     fun updateRV(newList: List<Film>) {
